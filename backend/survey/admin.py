b7c7a6aeff5c506d8cf4038839b8889ff579eeb7
x.karimjonov <khtkarimzhonov@gmail.com>
2023-04-28 14:19:58 +0500

    add modeladmins

--- backend/survey/admin.py
+++ backend/survey/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SurveyResult)
+class SurveyResultAdmin(models.SurveyResult):
+    pass
