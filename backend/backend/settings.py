905d26312e291c5a68dc36bbca0ca25986bc2d61
x.karimjonov <khtkarimzhonov@gmail.com>
2023-04-29 10:51:05 +0500

    postgis uninstall

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.gis",
+    # "django.contrib.gis",
     "django.contrib.postgres",
     "rest_framework_gis",
     "corsheaders",
