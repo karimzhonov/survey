342c7700f1e9b75ced25c4e9fdaa8666993e74c8
x.karimjonov <khtkarimzhonov@gmail.com>
2023-05-04 17:10:34 +0500

    fixes

--- frontend/src/apps/survey/views/SurveyPublic.vue
+++ frontend/src/apps/survey/views/SurveyPublic.vue
@@ -1,5 +1,6 @@
 <template>
     <div v-if="survey_active" id="surveyElement" style="display: inline-block; width: 100%;">
+      <h2 v-if="survey_response" class="survey-title">{{ survey_response.data.title ? survey_response.data.title[$i18n.locale] ?? survey_response.data.title.default : survey_response.name }}</h2>
         <survey params="survey: model"></survey>
         <div v-if="ended" class="sv-action__content flex justify-content-center">
           <input @click="start_again" type="button" :value="$t('Пройти опрос еще раз')" class="sd-btn sd-btn--action sd-navigation__complete-btn" :title="$t('Тестировать еще раз')">
@@ -28,13 +29,15 @@ export default {
   data() {
     return {
       survey_active: true,
-      ended: false
+      ended: false,
+      survey_response: null
     }
   },
   async mounted(){
     let survey_response = null
     try {
       survey_response = await $survey.public.get(this.id)
+      this.survey_response = survey_response
     } catch (e) {
       console.log(e);
       this.survey_active = false
@@ -83,3 +86,40 @@ export default {
 }
 
 </script>
+<style>
+.survey-title {
+  font-size: calc(4 * var(--base-unit, 8px));
+  font-weight: 700;
+  line-height: calc(5 * var(--base-unit, 8px));
+  color: var(--primary, #19b394);
+  background-color: var(--background, #fff);
+  border-top: 2px;
+  border-bottom: 2px;
+  border-color: var(--primary, #19b394);
+  text-align: center;
+  margin: 0;
+  position: sticky;
+  top: 70px;
+  z-index: 10;
+  padding-bottom: 20px;
+}
+.sd-title.sd-container-modern__title {
+  padding-top: 0;
+}
+@media screen and (min-width: 769px) {
+  .sd-header__text h3 {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 769px) {
+  .survey-title {
+    display: none;
+  }
+}
+
+.sd-progress.sd-body__progress.sd-body__progress--top {
+  display: none;
+}
+
+</style>
