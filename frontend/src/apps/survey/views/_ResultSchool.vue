37979fe45225dc4179f44b80479f19be713736f8
x.karimjonov <khtkarimzhonov@gmail.com>
2023-05-08 10:45:01 +0500

    Collapsable results

--- frontend/src/apps/survey/views/_ResultSchool.vue
+++ frontend/src/apps/survey/views/_ResultSchool.vue
@@ -1,5 +1,5 @@
 <template>
-    <Toolbar class="mb-4">
+    <Toolbar>
             <template #start>
                 <span class="p-input-icon-left">
                     <label for="start_date">{{ $t("Дата начало") }}</label>
@@ -20,11 +20,62 @@
             </template>
         </Toolbar>
     
-    <DataTable v-model:filters="filters" :value="results" showGridlines responsiveLayout="scroll" :globalFilterFields="['school']" :loading="loading">
-        <Column field="school" :header="$t('Школа')"></Column>
-        <Column field="value_9" :header="$t('9-класс')"></Column>
-        <Column field="value_11" :header="$t('11-класс')"></Column>
-        <Column field="sum" :header="$t('Общий')"></Column>
+    <DataTable v-model:filters="filters" :value="results" showGridlines responsiveLayout="scroll" :globalFilterFields="['school']" :loading="loading" v-model:expandedRows="expandedRows">
+        <Column expander style="width: 5rem" />
+        <Column field="region" :header="$t('Район')"></Column>
+        <Column field="value_9" :header="$t('9-класс, Численнсть')"></Column>
+        <Column field="v_9" :header="$t('9-класс, Участвовали')"></Column>
+        <Column field="v_9_per" :header="$t('9-класс, Охват (%)')">
+            <template #body="slotProps">{{ slotProps.data.v_9_per }} {{ slotProps.data.v_9_per ? "%" : "" }}</template>
+        </Column>
+        <Column field="value_11" :header="$t('11-класс, Численнсть')"></Column>
+        <Column field="v_11" :header="$t('11-класс, Участвовали')"></Column>
+        <Column field="v_11_per" :header="$t('11-класс, Охват (%)')">
+            <template #body="slotProps">{{ slotProps.data.v_11_per }} {{ slotProps.data.v_11_per ? "%" : "" }}</template>
+        </Column>
+        <Column field="summa" :header="$t('Итог, Численнсть')"></Column>
+        <Column field="sum" :header="$t('Итог, Участвовали')"></Column>
+        <Column field="sum_per" :header="$t('Итог, Охват (%)')">
+            <template #body="slotProps">{{ slotProps.data.sum_per }} {{ slotProps.data.sum_per ? "%" : "" }}</template>
+        </Column>
+        <ColumnGroup type="footer">
+            <Row>
+                <Column></Column>
+                <Column :footer="all.region"></Column>
+                <Column :footer="all.value_9" :header="$t('11-класс, Численнсть')"></Column>
+                <Column :footer="all.v_9" :header="$t('9-класс, Участвовали')"></Column>
+                <Column :footer="all.v_9_per" :header="$t('9-класс, Охват (%)')">
+                    <template #body="slotProps">{{ slotProps.data.v_9_per }} {{ slotProps.data.v_9_per ? "%" : "" }}</template>
+                </Column>
+                <Column :footer="all.value_11" :header="$t('11-класс, Численнсть')"></Column>
+                <Column :footer="all.v_11" :header="$t('11-класс, Участвовали')"></Column>
+                <Column :footer="all.v_11_per" :header="$t('11-класс, Охват (%)')">
+                    <template #body="slotProps">{{ slotProps.data.v_11_per }} {{ slotProps.data.v_11_per ? "%" : "" }}</template>
+                </Column>
+                <Column :footer="all.summa" :header="$t('Итог, Численнсть')"></Column>
+                <Column :footer="all.sum" :header="$t('Итог, Участвовали')"></Column>
+                <Column :footer="all.sum_per" :header="$t('Итог, Охват (%)')">
+                    <template #body="slotProps">{{ slotProps.data.sum_per }} {{ slotProps.data.sum_per ? "%" : "" }}</template>
+                </Column>
+            </Row>
+        </ColumnGroup>
+        <template #expansion="slotProps">
+            <div class="p-3">
+                <h5>Школы - {{ slotProps.data.region }}</h5>
+                <DataTable :value="slotProps.data.schools" >
+                    <Column field="school" :header="$t('Школа')"></Column>
+                    <Column field="value_9" :header="$t('9-класс, Численнсть')"></Column>
+                    <Column field="v_9" :header="$t('9-класс, Участвовали')"></Column>
+                    <Column field="v_9_per" :header="$t('9-класс, Охват (%)')"></Column>
+                    <Column field="value_9" :header="$t('11-класс, Численнсть')"></Column>
+                    <Column field="v_9" :header="$t('11-класс, Участвовали')"></Column>
+                    <Column field="v_9_per" :header="$t('11-класс, Охват (%)')"></Column>
+                    <Column field="summa" :header="$t('Итог, Численнсть')"></Column>
+                    <Column field="sum" :header="$t('Итог, Участвовали')"></Column>
+                    <Column field="sum_per" :header="$t('Итог, Охват (%)')"></Column>
+                </DataTable>
+            </div>
+        </template>
     </DataTable>
 </template>
 <script>
@@ -42,7 +93,9 @@ export default {
             loading: true,
             dates,
             active: 0,
-            filters: {global: { value: null, matchMode: FilterMatchMode.CONTAINS }}
+            filters: {global: { value: null, matchMode: FilterMatchMode.CONTAINS }},
+            expandedRows: [],
+            all: {}
         }
     },
     async mounted() {
@@ -50,38 +103,115 @@ export default {
     },
     methods: {
         async change_tab() {
+            let plan = await fetch("/json/schools.json")
+            plan = await plan.json()
+            delete plan["NaN"]
             this.loading = true
             const results_9 = await axios.get(`/api/survey/survey-public/${this.ids[0]}/result/`, {params: {...this.dates_to_iso_dict(this.dates)}})
             const results_11 = await axios.get(`/api/survey/survey-public/${this.ids[1]}/result/`, {params: {...this.dates_to_iso_dict(this.dates)}})
-            const school_results = {}
+
             for (let r of results_9.data) {
-                if (school_results[Object.values(r.data)[1]]) {
-                    school_results[Object.values(r.data)[1]].value_9 += 1
-                } else {
-                    school_results[Object.values(r.data)[1]] = {
-                        value_11: 0,
-                        value_9: 1
+                for (let region in plan) {
+                    for (let school in plan[region]) {
+                        console.log(Object.values(r.data)[1].replace("-мактаб", "") == school, Object.values(r.data)[1].replace("-мактаб", ""), school, region);
+                        if (
+                            school == Object.values(r.data)[1] || 
+                            school == Object.values(r.data)[1].replace("-мактаб", "") ||
+                            school == Object.values(r.data)[1].replace("мактаб", "") ||
+                            school == Object.values(r.data)[1].replace(" мактаб", "") ||
+
+                            school.replace("-мактаб", "") == Object.values(r.data)[1] ||
+                            school.replace("мактаб", "") == Object.values(r.data)[1] ||
+                            school.replace(" мактаб", "") == Object.values(r.data)[1] ||
+
+                            school.replace("-maktab", "") == Object.values(r.data)[1] ||
+                            school.replace("maktab", "") == Object.values(r.data)[1] ||
+                            school.replace(" maktab", "") == Object.values(r.data)[1]
+                        ) {
+                            if (plan[region][school]["v_9"]) {
+                                plan[region][school]["v_9"] += 1
+                            } else {
+                                plan[region][school]["v_9"] = 1
+                            }
+                        }
                     }
                 }
             }
 
             for (let r of results_11.data) {
-                if (school_results[Object.values(r.data)[1]]) {
-                    school_results[Object.values(r.data)[1]].value_11 += 1
-                } else {
-                    school_results[Object.values(r.data)[1]] = {
-                        value_11: 1,
-                        value_9: 0
+                for (let region in plan) {
+                    for (let school in plan[region]) {
+                        if (                            
+                            school == Object.values(r.data)[1] || 
+                            school == Object.values(r.data)[1].replace("-мактаб", "") ||
+                            school == Object.values(r.data)[1].replace("мактаб", "") ||
+                            school == Object.values(r.data)[1].replace(" мактаб", "") ||
+
+                            school.replace("-мактаб", "") == Object.values(r.data)[1] ||
+                            school.replace("мактаб", "") == Object.values(r.data)[1] ||
+                            school.replace(" мактаб", "") == Object.values(r.data)[1] ||
+
+                            school.replace("-maktab", "") == Object.values(r.data)[1] ||
+                            school.replace("maktab", "") == Object.values(r.data)[1] ||
+                            school.replace(" maktab", "") == Object.values(r.data)[1]
+                        ) {
+                            if (plan[region][school]["v_11"]) {
+                                plan[region][school]["v_11"] += 1
+                            } else {
+                                plan[region][school]["v_11"] = 1
+                            }
+                        }
                     }
                 }
             }
-
-            const ordered = Object.keys(school_results).sort().reduce((obj, key) => { 
-                obj.push({"school": key, ...school_results[key], sum: school_results[key].value_11 + school_results[key].value_9})
+            const ordered = Object.keys(plan).sort().reduce((obj, key) => { 
+                const d = {
+                    "region": key, 
+                    "schools": Object.keys(plan[key]).sort().reduce((acc, school) => {
+                        const d = {
+                            "school": school, ...plan[key][school],
+                        }
+                        d.v_9_per = d.v_9 ? Math.round(d.v_9 / d.value_9 * 10000) / 100 : ""
+                        d.v_11_per = d.v_11 ? Math.round(d.v_11 / d.value_11 * 10000) / 100 : ""
+                        d.summa = d.value_9 + d.value_11
+                        d.sum = d.v_9 ? d.v_9 : 0 + d.v_11 ? d.v_11 : 0
+                        d.sum = d.sum ? d.sum : ""
+                        d.sum_per = d.sum ? Math.round(d.sum / d.summa * 10000) / 100 : ""
+                        acc.push(d)
+                        return acc
+                    }, []),
+                    v_9: Object.values(plan[key]).reduce((a, v) => {a += v.v_9 ?? 0; return a}, 0),
+                    value_9: Object.values(plan[key]).reduce((a, v) => {a += v.value_9 ?? 0; return a}, 0),
+                    v_11: Object.values(plan[key]).reduce((a, v) => {a += v.v_11 ?? 0; return a}, 0),
+                    value_11: Object.values(plan[key]).reduce((a, v) => {a += v.value_11 ?? 0; return a}, 0),
+                } 
+                d.v_9_per = d.v_9 ? Math.round(d.v_9 / d.value_9 * 10000) / 100 : ""
+                d.v_11_per = d.v_11 ? Math.round(d.v_11 / d.value_11 * 10000) / 100 : ""
+                d.summa = d.value_9 + d.value_11
+                d.sum = d.v_9 ? d.v_9 : 0 + d.v_11 ? d.v_11 : 0
+                d.sum = d.sum ? d.sum : ""
+                d.sum_per = d.sum ? Math.round(d.sum / d.summa * 10000) / 100 : ""
+                obj.push(d)
                 return obj;
             }, []);
+            const all = {
+                region: "Общий итог",
+                v_9: Object.values(ordered).reduce((a, v) => {a += v.v_9 ?? 0; return a}, 0),
+                value_9: Object.values(ordered).reduce((a, v) => {a += v.value_9 ?? 0; return a}, 0),
+                v_11: Object.values(ordered).reduce((a, v) => {a += v.v_11 ?? 0; return a}, 0),
+                value_11: Object.values(ordered).reduce((a, v) => {a += v.value_11 ?? 0; return a}, 0),
+            }
+            all.v_9_per = all.v_9 ? Math.round(all.v_9 / all.value_9 * 10000) / 100 : ""
+            all.v_11_per = all.v_11 ? Math.round(all.v_11 / all.value_11 * 10000) / 100 : ""
+            all.summa = all.value_9 + all.value_11
+            all.sum = all.v_9 ? all.v_9 : 0 + all.v_11 ? all.v_11 : 0
+            all.sum = all.sum ? all.sum : ""
+            all.sum_per = all.sum ? Math.round(all.sum / all.summa * 10000) / 100 : ""
+            this.all = all
             this.results = ordered
+            console.log(ordered);
             this.loading = false
+            
         },
         async change_dates() {
             if (this.dates[1]) {
---
+++ test.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+xlsx = pd.read_excel("Boriladigan maktablar (1).xlsx")
+
+regions = defaultdict(dict)
+
+for row in xlsx.iloc:
+    regions[row["Туман (шаҳар)"]][row["мактаб"]] = {
+        "value_9": row["9-синф  Ўқувчилар сони"] if not np.isnan(row["9-синф  Ўқувчилар сони"]) else None,
+        "value_11": row["11-синф  Ўқувчилар сони"] if not np.isnan(row["11-синф  Ўқувчилар сони"]) else None
+    }
+
+with open("schools.json", "w", encoding="utf8") as file:
+    json.dump(regions, file, ensure_ascii=False)
