bcb147188469429bf35756507e46af8627ee06b9
x.karimjonov <khtkarimzhonov@gmail.com>
2023-05-12 17:27:55 +0500

    fixes

--- frontend/src/apps/survey/views/_ResultSchool.vue
+++ frontend/src/apps/survey/views/_ResultSchool.vue
@@ -22,13 +22,15 @@
     <DataTable :value="results" showGridlines responsiveLayout="scroll" :loading="loading" v-model:expandedRows="expandedRows">
         <Column expander style="width: 5rem" />
         <Column field="region" :header="$t('Район')"></Column>
-        <Column field="value_9" :header="$t('9-класс, Численнсть')"></Column>
+        <Column field="survey_count" :header="$t('Участвовали')"></Column>
+        <Column field="school_count" :header="$t('Кол-во школ')"></Column>
+        <Column field="value_9" :header="$t('9-класс, Численость')"></Column>
         <Column field="v_9" :header="$t('9-класс, Участвовали')"></Column>
         <Column field="v_9_per" :header="$t('9-класс, Охват (%)')"></Column>
-        <Column field="value_11" :header="$t('11-класс, Численнсть')"></Column>
+        <Column field="value_11" :header="$t('11-класс, Численость')"></Column>
         <Column field="v_11" :header="$t('11-класс, Участвовали')"></Column>
         <Column field="v_11_per" :header="$t('11-класс, Охват (%)')"></Column>
-        <Column field="summa" :header="$t('Итог, Численнсть')"></Column>
+        <Column field="summa" :header="$t('Итог, Численость')"></Column>
         <Column field="sum" :header="$t('Итог, Участвовали')"></Column>
         <Column field="sum_per" :header="$t('Итог, Охват (%)')"></Column>
         <ColumnGroup type="footer">
@@ -72,13 +74,13 @@
 
                 <DataTable :value="slotProps.data.schools" v-model:filters="filters[slotProps.data.region]" showGridlines responsiveLayout="scroll" :globalFilterFields="['school']">
                     <Column field="school" :header="$t('Школа')"></Column>
-                    <Column field="value_9" :header="$t('9-класс, Численнсть')"></Column>
+                    <Column field="value_9" :header="$t('9-класс, Численость')"></Column>
                     <Column field="v_9" :header="$t('9-класс, Участвовали')"></Column>
                     <Column field="v_9_per" :header="$t('9-класс, Охват (%)')"></Column>
-                    <Column field="value_11" :header="$t('11-класс, Численнсть')"></Column>
+                    <Column field="value_11" :header="$t('11-класс, Численость')"></Column>
                     <Column field="v_11" :header="$t('11-класс, Участвовали')"></Column>
                     <Column field="v_11_per" :header="$t('11-класс, Охват (%)')"></Column>
-                    <Column field="summa" :header="$t('Итог, Численнсть')"></Column>
+                    <Column field="summa" :header="$t('Итог, Численость')"></Column>
                     <Column field="sum" :header="$t('Итог, Участвовали')"></Column>
                     <Column field="sum_per" :header="$t('Итог, Охват (%)')"></Column>
                 </DataTable>
@@ -212,6 +214,8 @@ export default {
                     v_11: Object.values(plan[key]).reduce((a, v) => {a += v.v_11 ?? 0; return a}, 0),
                     value_11: Object.values(plan[key]).reduce((a, v) => {a += v.value_11 ?? 0; return a}, 0),
                 } 
+                d.school_count = plan[key].length
+                d.survey_count = d.schools.reduce((a, v) => {a += v.v_9 + v.v_1 > 10 ? 1 : 0; return a}, 0)
                 d.v_9_per = `${d.v_9 ? Math.round(d.v_9 / d.value_9 * 10000) / 100 : ""}%`
                 d.v_11_per = `${d.v_11 ? Math.round(d.v_11 / d.value_11 * 10000) / 100 : ""}%`
                 d.summa = d.value_9 + d.value_11
@@ -271,13 +275,13 @@ export default {
                     const row_excel = {}
                     row_excel[`${this.$t('Район')}`] = region.region
                     row_excel[`${this.$t('Школа')}`] = school.school
-                    row_excel[`${this.$t('9-класс, Численнсть')}`] = school.value_9
+                    row_excel[`${this.$t('9-класс, Численость')}`] = school.value_9
                     row_excel[`${this.$t('9-класс, Участвовали')}`] = school.v_9
                     row_excel[`${this.$t('9-класс, Охват (%)')}`] = school.v_9_per
-                    row_excel[`${this.$t('11-класс, Численнсть')}`] = school.value_11
+                    row_excel[`${this.$t('11-класс, Численость')}`] = school.value_11
                     row_excel[`${this.$t('11-класс, Участвовали')}`] = school.v_11
                     row_excel[`${this.$t('11-класс, Охват (%)')}`] = school.v_11_per
-                    row_excel[`${this.$t('Итог, Численнсть')}`] = school.summa
+                    row_excel[`${this.$t('Итог, Численость')}`] = school.summa
                     row_excel[`${this.$t('Итог, Участвовали')}`] = school.sum
                     row_excel[`${this.$t('Итог, Охват (%)')}`] = school.sum_per
                     data.push(row_excel)
@@ -286,26 +290,26 @@ export default {
             let row_excel = {}
             row_excel[`${this.$t('Район')}`] = "Общий результат"
             row_excel[`${this.$t('Школа')}`] = ""
-            row_excel[`${this.$t('9-класс, Численнсть')}`] = this.all.value_9
+            row_excel[`${this.$t('9-класс, Численость')}`] = this.all.value_9
             row_excel[`${this.$t('9-класс, Участвовали')}`] = this.all.v_9
             row_excel[`${this.$t('9-класс, Охват (%)')}`] = this.all.v_9_per
-            row_excel[`${this.$t('11-класс, Численнсть')}`] = this.all.value_11
+            row_excel[`${this.$t('11-класс, Численость')}`] = this.all.value_11
             row_excel[`${this.$t('11-класс, Участвовали')}`] = this.all.v_11
             row_excel[`${this.$t('11-класс, Охват (%)')}`] = this.all.v_11_per
-            row_excel[`${this.$t('Итог, Численнсть')}`] = this.all.summa
+            row_excel[`${this.$t('Итог, Численость')}`] = this.all.summa
             row_excel[`${this.$t('Итог, Участвовали')}`] = this.all.sum
             row_excel[`${this.$t('Итог, Охват (%)')}`] = this.all.sum_per
             data.push(JSON.parse(JSON.stringify(row_excel)))
             row_excel = {}
             row_excel[`${this.$t('Район')}`] = "Общий итог"
             row_excel[`${this.$t('Школа')}`] = ""
-            row_excel[`${this.$t('9-класс, Численнсть')}`] = this.all_all.value_9
+            row_excel[`${this.$t('9-класс, Численость')}`] = this.all_all.value_9
             row_excel[`${this.$t('9-класс, Участвовали')}`] = this.all_all.v_9
             row_excel[`${this.$t('9-класс, Охват (%)')}`] = this.all_all.v_9_per
-            row_excel[`${this.$t('11-класс, Численнсть')}`] = this.all_all.value_11
+            row_excel[`${this.$t('11-класс, Численость')}`] = this.all_all.value_11
             row_excel[`${this.$t('11-класс, Участвовали')}`] = this.all_all.v_11
             row_excel[`${this.$t('11-класс, Охват (%)')}`] = this.all_all.v_11_per
-            row_excel[`${this.$t('Итог, Численнсть')}`] = this.all_all.summa
+            row_excel[`${this.$t('Итог, Численость')}`] = this.all_all.summa
             row_excel[`${this.$t('Итог, Участвовали')}`] = this.all_all.sum
             row_excel[`${this.$t('Итог, Охват (%)')}`] = this.all_all.sum_per
             data.push(row_excel)
